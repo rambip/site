9f1e7e926e4e0a14713a39e6629668bf8984ac35
rambip <apero1808@gmail.com>
2022-07-12 23:28:10 +0200

    typos again

--- portfolio/entrypoint.sh
+++ portfolio/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cp -r $1/data /cv/_data
-cp -r $1./img /cv/assets/img
-cp -r $1./posts /cv/_posts
+cp -r $1/img /cv/assets/img
+cp -r $1/posts /cv/_posts
 
 cd /cv
-/usr/gem/bin/jekyll build -s /cv
+/usr/gem/bin/jekyll build -s /cv -d ./
