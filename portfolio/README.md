b34dbb387107ecd61fcc9ef980a058797fa41f7f
rambip <apero1808@gmail.com>
2022-07-12 22:57:34 +0200

    entrypoint for docker

---
+++ portfolio/README.md
@@ -0,0 +1,3 @@
+doc for github actions:
+
+https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions
---
+++ portfolio/entrypoint.sh
@@ -0,0 +1,10 @@
+#/usr/bin/sh
+
+# TODO: environment variable for location of folders
+
+cp -r ./data /cv/_data
+cp -r ./imgs /cv/assets/img
+cp -r ./posts /cv/_posts
+
+cd /cv
+/usr/gem/bin $1
