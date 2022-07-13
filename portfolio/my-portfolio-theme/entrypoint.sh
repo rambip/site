230d1b05701de5967c7ce774977018a58aee15c2
rambip <apero1808@gmail.com>
2022-07-13 20:34:18 +0200

    fixed error

--- portfolio/my-portfolio-theme/entrypoint.sh
+++ portfolio/my-portfolio-theme/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 pwd
+
+echo "content of data folder":
 ls -la
-ln -s $PWD/data /cv/_data
-ln -s $PWD/img /cv/assets/img
-ln -s $PWD/posts /cv/_posts
+
+ln -s $SOURCE_SIR/data /cv/_data
+ln -s $SOURCE_DIR/img /cv/assets/img
+ln -s $SOURCE_DIR/posts /cv/_posts
 
 mkdir /cv/_site
-mkdir $PWD/_site
-ln -s /cv/_site $PWD/_site
+mkdir $DEST_DIR/_site
+ln -s /cv/_site $DEST_DIR/_site
 
 cd /cv
 /usr/gem/bin/jekyll $@
