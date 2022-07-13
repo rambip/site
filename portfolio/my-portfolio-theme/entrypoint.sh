b5600648879ee74adc8da9157bf3c2082cf8fef0
rambip <apero1808@gmail.com>
2022-07-13 20:49:52 +0200

    fixed error

--- portfolio/my-portfolio-theme/entrypoint.sh
+++ portfolio/my-portfolio-theme/entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-pwd
+test -z "$SOURCE_DIR" && SOURCE_DIR=/srv/jekyll
 
-echo "content of data folder":
-ls -la
-
-ln -s $SOURCE_SIR/data /cv/_data
+# link ressources needed to build page in the right folder
+ln -s $SOURCE_DIR/data /cv/_data
 ln -s $SOURCE_DIR/img /cv/assets/img
 ln -s $SOURCE_DIR/posts /cv/_posts
 
-mkdir /cv/_site
-mkdir $DEST_DIR/_site
-ln -s /cv/_site $DEST_DIR/_site
+mkdir /srv/jekyll/_site
+mkdir /cv/site
+ln -s /cv/_site /srv/jekyll/_site
 
 cd /cv
 /usr/gem/bin/jekyll $@
