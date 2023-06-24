61abaa756e5c603820788d9a90c068f84ddac922
rambip <apero1808@gmail.com>
2023-06-24 15:47:51 +0200

    updated dependencies

--- flake.nix
+++ flake.nix
@@ -2,20 +2,22 @@
   description = "the flake for the website of rambip";
 
   inputs = {
-      julia-source.url = "github:rambip/web-julia";
-      perfect-clear-source.url = "github:rambip/web-perfect-clear";
-      web-compress-source.url = "github:rambip/web-compress";
+      julia.url = "github:rambip/web-julia";
+      perfect-clear.url = "github:rambip/web-perfect-clear";
+      web-compress.url = "github:rambip/web-compress";
       utils.url   = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, julia-source, perfect-clear-source, web-compress-source, utils }: 
-  with utils.lib; eachSystem allSystems (
+  outputs = { self, nixpkgs, julia, perfect-clear, web-compress, utils }: 
+  with utils.lib; eachSystem [system.x86_64-linux] (
     system:
         with import nixpkgs {inherit system;};
-        { packages.default = callPackage ./website.nix {
-            julia = julia-source.defaultPackage."${system}";
-            perfect-clear = perfect-clear-source.defaultPackage."${system}";
-            web-compress = web-compress-source.defaultPackage."${system}";
-        }; }
+        { 
+            packages.default = callPackage ./website.nix {
+                julia = julia.packages."${system}".default;
+                perfect-clear = perfect-clear.packages."${system}".default;
+                web-compress = web-compress.packages."${system}".default;
+            }; 
+        }
   );
 }
